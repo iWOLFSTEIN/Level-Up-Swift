f5862c7bd9eabbeb2084226122c410e0e1c8bced
Talha Ashraf <talha.ashraf@brainxtech.com>
2023-05-10 12:46:57 +0500

    Initial Commit

---
+++ Demo App 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo App 2
+//
+//  Created by BrainX Technologies on 10/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
