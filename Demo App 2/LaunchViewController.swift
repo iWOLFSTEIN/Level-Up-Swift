7cfb877771987f25ed5df93aafbda21a9051c91f
Talha Ashraf <talha.ashraf@brainxtech.com>
2023-05-11 15:15:21 +0500

    login screen completed

---
+++ Demo App 2/LaunchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LuanchViewController.swift
+//  Demo App 2
+//
+//  Created by BrainX Technologies on 11/05/2023.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+}
