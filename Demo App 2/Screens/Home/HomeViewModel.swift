b96ae9348c1926534f15d8c1c20fa05f7abcc1cc
Talha Ashraf <talha.ashraf@brainxtech.com>
2023-05-29 20:28:18 +0500

    finished

--- Demo App 2/Screens/Home/HomeViewModel.swift
+++ Demo App 2/Screens/Home/HomeViewModel.swift
@@ -1,35 +1,3 @@
-//import Foundation
-//
-//class HomeViewModel {
-//    var generalRepository: QuoteRespository!
-//    var bind: (() -> Void) = {}
-//    var username: String? = nil
-//    var quote: Quote? {
-//        didSet {
-//            self.bind()
-//        }
-//    }
-//
-//    init() {
-//        let alamofireAPIManager = AlamofireAPIManager(authProvider: UserDefaultAuth())
-//        self.generalRepository = QuoteRespository(apiManager: alamofireAPIManager)
-//        getQuoteResponse()
-//    }
-//
-//    func getQuoteResponse() {
-//        generalRepository.getQuote { [weak self] (result: Result<Quote, Error>) in
-//            switch result {
-//            case .success(let quote):
-//                self?.quote = quote
-//            case .failure(let error):
-//                print("Error Getting Quote: \(error)")
-//                showErrorAlert(title: "Error", message: "An error occurred while fetching quote")
-//            }
-//        }
-//    }
-//
-//
-//}
 import Combine
 import Foundation
 
